214eaf2ffd2de9a655d349ff2a57e83c9057531e
Maulana Adam Sahid <masadamsahid@gmail.com>
2021-11-10 05:09:19 +0700

    add model from rzyas

--- js/main.js
+++ js/main.js
@@ -5,18 +5,15 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
 camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame( animate );
-
-  cube.rotation.y += 0.02;
-
   renderer.render( scene, camera );
 }
 animate();
