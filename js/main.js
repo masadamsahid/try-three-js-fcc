d1e1a7490f3889158e87045cf7e743804f4ee008
Maulana Adam Sahid <masadamsahid@gmail.com>
2021-11-10 10:55:49 +0700

    Ada yang error (╯°□°）╯︵ ┻━┻

--- js/main.js
+++ js/main.js
@@ -5,12 +5,20 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
 // const geometry = new THREE.BoxGeometry();
 // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 // const cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
 camera.position.z = 5;
+
+const loader = new THREE.GLTFLoader()
+loader.load("../model/scene.gltf", (gltf) => {
+  scene.add(gltf.scene);
+});
 
 function animate() {
   requestAnimationFrame( animate );
